80ed3f7d11f5d16b4507b68b536004503b9e9771
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-04-21 17:56:15 -0500

    adding & removing sections around

--- gmap.js
+++ gmap.js
@@ -1,1 +1,10 @@
+var map;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 23.229476, lng: 72.674003},
+    zoom: 8
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', init);
