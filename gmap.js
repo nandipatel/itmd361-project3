635b0623048df65629d620167a31ae7f0da434f3
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-04-21 18:15:12 -0500

    adding to markers on 3 locations, based on page2.

--- gmap.js
+++ gmap.js
@@ -1,10 +1,42 @@
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 23.229476, lng: 72.674003},
-    zoom: 8
-  });
+  map = new google.maps.Map(document.getElementById('map');
+
+  var mainLoc = new google.maps.LatLng (27.1767, 78.0081);
+
+  var pearlm = new google.maps.LatLng (27.1804, 78.0216);
+
+  var tajm = new google.maps.LatLng (27.1751, 78.0421);
+
+  var tomb = new google.maps.LatLng (27.2206, 77.9505);
+
+  var mapOptions = {
+    center: mainLoc,
+    zoom: 12
+  };
+
+  var marker1 = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: pearlm
+  )};
+  marker1.addListener('click', toggleBounce);
+
+  var marker2 = new google.maps.Marker({
+    map: map
+    animation: google.maps.Animation.DROP,
+    position: tajm
+  )};
+  marker2.addListener('click', toggleBounce);
+
+  var marker3 = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: tomb
+  )};
+  marker3.addListener('click', toggleBounce);
+
 }
 
 google.maps.event.addDomListener(window, 'load', init);
